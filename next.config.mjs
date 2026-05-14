02399b502f3fa1a1f3d6cf58b4ca6773135c5b08
lieoxa <adrielfelixposuma@gmai.com>
2026-05-15 05:18:28 +0700

    Configure Next.js for GitHub Pages Deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/code-gabut',
+  images: {
+    unoptimized: true,
+  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, encoding: false, path: false, crypto: false };
     return config;
